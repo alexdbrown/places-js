997d851b103be7a54c869952001d1c414ce60fdd
Alexandra Brown & Deron Johnson <student@epicodus.com>
2015-09-14 13:50:52 -0700

    Change html to allow multiple highlights

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,17 @@ $(document).ready(function() {
     event.preventDefault();
 
     var inputPlaceName = $("input#new-place-name").val();
-    var inputHighlight = $("input#new-place-highlight").val();
     var inputTime = $("input#new-place-time").val();
     var inputDuration = $("input#new-place-duration").val();
 
-    var newPlace = { placeName: inputPlaceName, placeHighlight: inputHighlight, placeTime: inputTime, placeDuration: inputDuration };
+    var newPlace = { placeName: inputPlaceName, placeTime: inputTime, placeDuration: inputDuration,  placeHighlights: [] };
+
+    $(".new-highlight").each(function() {
+      var inputHighlight = $(this).find("input#highlight").val();
+
+      var newHighlight = { highlight: inputHighlight };
+      newPlace.placeHighlights.push(newHighlight);
+    });
 
     $("ul#places").append("<li><span class='place'>" + newPlace.placeName + "</span></li>");
 
@@ -16,15 +22,15 @@ $(document).ready(function() {
 
       $("#show-place h2").text(newPlace.placeName);
       $(".place-name").text(newPlace.placeName);
-      $(".place-highlight").text(newPlace.placeHighlight);
       $(".place-time").text(newPlace.placeTime);
       $(".place-duration").text(newPlace.placeDuration);
+      $(".place-highlight").text(newPlace.placeHighlight);
 
     });
 
     $("input#new-place-name").val("");
-    $("input#new-place-highlight").val("");
     $("input#new-place-time").val("");
     $("input#new-place-duration").val("");
+    $("input#new-place-highlight").val("");
   });
 });
